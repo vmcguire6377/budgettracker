3a7052a3f683d34ec6c0c52e0cd9d5130bb0fe2b
VeeMcGee <vmcguire6377@outlook.com>
2021-07-26 21:55:30 -0600

    correcting syntax error

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,7 @@
 let transactions = [];
 let myChart;
 
-fetch("/api/entry")
+fetch("/api/transaction")
   .then(response => {
     return response.json();
   })
--- public/service-worker.js
+++ public/service-worker.js
@@ -70,6 +70,7 @@ self.addEventListener('fetch', function (e) {
         );
         return;
     }
+})
 
 
 
